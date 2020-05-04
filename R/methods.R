bb52fcb7a48a0978a504d9aee6f8c34aa03b3b78
Andrew <andrew.decarlo@mtna.us>
2020-05-04 14:51:04 -0400

    updated so that multivalued arguements can be passed in as vectors.

--- R/methods.R
+++ R/methods.R
@@ -59,8 +59,8 @@ setGeneric("getCatalog", function(server, catalogId)
 #' Get Catalog
 #'
 #' The getCatalog(server, catalogId) method can be used to get the catalog with the provided ID from the provided rds.server.
-#' 
-#' @import jsonlite 
+#'
+#' @import jsonlite
 #' @param server The server to query for the catalog.
 #' @param catalogId The ID of the desired catalog.
 setMethod("getCatalog", signature("rds.server", "character"), function(server, catalogId) {
@@ -110,8 +110,8 @@ setMethod("getCatalog", signature("rds.server", "character"), function(server, c
 #' Get Data Products
 #'
 #' The getDataProducts(catalog) method can be used to retrieve all the data products from the provided catalog.
-#' 
-#' @import jsonlite 
+#'
+#' @import jsonlite
 #' @param catalog The catalog to query for the data products.
 #' @name getDataProducts
 #' @rdname getDataProducts
@@ -122,8 +122,8 @@ setGeneric("getDataProducts", function(catalog)
 #' Get Data Products
 #'
 #' The getDataProducts(catalog) method can be used to retrieve all the data products from the provided catalog.
-#' 
-#' @import jsonlite 
+#'
+#' @import jsonlite
 #' @param catalog The catalog to query for the data products.
 setMethod("getDataProducts", signature("rds.catalog"), function(catalog) {
   # Set up the request URL
@@ -175,8 +175,8 @@ setGeneric("getDataProduct", function(catalog, dataProductId)
 #' Get Data Product
 #'
 #' The getDataProduct(catalog, dataProductId) method can be used to get the data product with the provided ID from the provided rds.catalog.
-#' 
-#' @import jsonlite 
+#'
+#' @import jsonlite
 #' @param catalog The catalog to query for the data product.
 #' @param dataProductId The ID of the desired data product.
 setMethod("getDataProduct", signature("rds.catalog", "character"), function(catalog, dataProductId) {
@@ -264,8 +264,8 @@ setGeneric("getVariables", function(dataProduct,
 #' Get Variables
 #'
 #' The getVariables(dataProduct) method can be used to get the variable summaries of all the with the provided ID from the provided rds.dataProduct.
-#' 
-#' @import jsonlite 
+#'
+#' @import jsonlite
 #' @param dataProduct The rds.dataProduct to query for the variable.
 #' @param cols An optional variable query following the RDS column syntax. If left NULL all the variable will be returned.
 #' @param collimit An optional limit to the variables returned. If left NULL no limit will be applied.
@@ -345,8 +345,8 @@ setGeneric("getVariable", function(dataProduct, variableId)
 #' Get Variable
 #'
 #' The getVariable(dataProduct, variableId) method can be used to get the variable with the provided ID from the provided rds.dataProduct.
-#' 
-#' @import jsonlite 
+#'
+#' @import jsonlite
 #' @param dataProduct The rds.dataProduct to query for the variable.
 #' @param variableId The ID of the desired variable.
 setMethod("getVariable", signature("rds.dataProduct", "character"), function(dataProduct, variableId) {
@@ -377,7 +377,7 @@ setMethod("getVariable", signature("rds.dataProduct", "character"), function(dat
     if (!is.null(variable$frequencies)) {
       for (i in 1:nrow(variable$frequencies$sets)) {
         # Get the set to work with
-        frequencySet <- variable$frequencies$sets[i, ]
+        frequencySet <- variable$frequencies$sets[i,]
         
         # Weight properties
         weighted <- frequencySet$weighted
@@ -385,7 +385,7 @@ setMethod("getVariable", signature("rds.dataProduct", "character"), function(dat
         
         # Set up the data frame
         setValues <- names(frequencySet$map)
-        setFrequencies <- frequencySet$map[1,]
+        setFrequencies <- frequencySet$map[1, ]
         frequencyDf <- data.frame()
         for (freqIndex in 1:length(setValues)) {
           value <- setValues[freqIndex]
@@ -494,8 +494,8 @@ setGeneric("getClassifications", function(dataProduct,
 #' Get Classifications
 #'
 #' The getClassifications(dataProduct) method can be used to retrieve the descriptive information about all the classifications that are used in the data product.
-#' 
-#' @import jsonlite 
+#'
+#' @import jsonlite
 #' @param dataProduct The rds.dataProduct to query for classifications.
 #' @param limit Specifies the number of classifications to return.
 #' @param offset Specifies the starting index of the classifications.
@@ -562,21 +562,23 @@ setMethod("getClassifications", signature("rds.dataProduct"), function(dataProdu
 #' @exportMethod getClassification
 setGeneric("getClassification", function(dataProduct,
                                          classificationId,
-                                                               limit = 1000,
+                                         limit = 1000,
                                          offset = 0)
   standardGeneric("getClassification"))
 
 #' Get Classification
 #'
 #' The getClassification(dataProduct, classificationId) method can be used to get the classification with the provided ID from the provided rds.dataProduct.
-#' 
-#' @import jsonlite 
+#'
+#' @import jsonlite
 #' @param dataProduct The rds.dataProduct to query for the classification
 #' @param classificationId The ID or URI of the desired classification
 #' @param limit Specifies the number of codes to return.
 #' @param offset Specifies the starting index of the codes.
-setMethod("getClassification", signature("rds.dataProduct", "character"), function(dataProduct, classificationId, limit=1000, offset=0) {
-
+setMethod("getClassification", signature("rds.dataProduct", "character"), function(dataProduct,
+                                                                                   classificationId,
+                                                                                   limit = 1000,
+                                                                                   offset = 0) {
   # Get the catalog from the data product
   catalog <- dataProduct@catalog
   
@@ -671,23 +673,23 @@ setMethod("getClassification", signature("rds.dataProduct", "character"), functi
 #' @rdname rds.select
 #' @exportMethod rds.select
 setGeneric("rds.select", function(dataProduct,
-                              limit = NULL,
-                              offset = NULL,
-                              cols = NULL,
-                              colLimit = NULL,
-                              colOffset = NULL,
-                              count = FALSE,
-                              orderby = NULL,
-                              where = NULL,
-                              inject = FALSE,
-                              autoPage = TRUE)
-           standardGeneric("rds.select"))
+                                  limit = NULL,
+                                  offset = NULL,
+                                  cols = NULL,
+                                  colLimit = NULL,
+                                  colOffset = NULL,
+                                  count = FALSE,
+                                  orderby = NULL,
+                                  where = NULL,
+                                  inject = FALSE,
+                                  autoPage = TRUE)
+  standardGeneric("rds.select"))
 
 #' Select
 #'
 #' The rds.select(dataProduct) method is used to access the record level data of the data product. In the explorer and through the API the total number of cells is limited to 10000 cells. This is done to keep a small and manageable amount of information going over the network. Be aware that by default the select method will perform numerous calls to build up a complete dataset. If this is not desired remember to set autoPage=FALSE.
-#' 
-#' @import jsonlite urltools 
+#'
+#' @import jsonlite urltools
 #' @param dataProduct The dataProduct whose data is desired.
 #' @param limit Specifies the number of records to return.
 #' @param offset Specifies the starting index of the records.
@@ -700,16 +702,16 @@ setGeneric("rds.select", function(dataProduct,
 #' @param inject Specifies if metadata should be injected into the data frame. If true and there are classifications available the columns codes will be replaced with code values. Defaults to FALSE
 #' @param autoPage If set to true multiple queries will be sent to the RDS server in order to compile the complete data set.
 setMethod("rds.select", signature("rds.dataProduct"), function(dataProduct,
-                                                           limit = 20,
-                                                           offset = 0,
-                                                           cols = NULL,
-                                                           colLimit = NULL,
-                                                           colOffset = NULL,
-                                                           count = FALSE,
-                                                           orderby = NULL,
-                                                           where = NULL,
-                                                           inject = FALSE,
-                                                           autoPage = TRUE) {
+                                                               limit = 20,
+                                                               offset = 0,
+                                                               cols = NULL,
+                                                               colLimit = NULL,
+                                                               colOffset = NULL,
+                                                               count = FALSE,
+                                                               orderby = NULL,
+                                                               where = NULL,
+                                                               inject = FALSE,
+                                                               autoPage = TRUE) {
   # Flag indicating that we need to run another query
   query = TRUE
   metadata = TRUE
@@ -762,12 +764,14 @@ setMethod("rds.select", signature("rds.dataProduct"), function(dataProduct,
     }
     
     if (!is.null(cols)) {
-      select <- paste(select,
-                      paramPrefix,
-                      "cols=",
-                      url_encode(cols),
-                      sep = "",
-                      collapse = NULL)
+      select <- paste(
+        select,
+        paramPrefix,
+        "cols=",
+        url_encode(paste(cols, collapse = ",")),
+        sep = "",
+        collapse = NULL
+      )
       paramPrefix <- "&"
     }
     
@@ -812,7 +816,7 @@ setMethod("rds.select", signature("rds.dataProduct"), function(dataProduct,
           select,
           paramPrefix,
           "orderby=",
-          url_encode(orderby),
+          url_encode(paste(orderby, collapse = ",")),
           sep = "",
           collapse = NULL
         )
@@ -820,12 +824,14 @@ setMethod("rds.select", signature("rds.dataProduct"), function(dataProduct,
     }
     
     if (!is.null(where))  {
-      select <- paste(select,
-                      paramPrefix,
-                      "where=",
-                      url_encode(where),
-                      sep = "",
-                      collapse = NULL)
+      select <- paste(
+        select,
+        paramPrefix,
+        "where=",
+        url_encode(where),
+        sep = "",
+        collapse = NULL
+      )
       paramPrefix <- "&"
     }
     
@@ -879,7 +885,7 @@ setMethod("rds.select", signature("rds.dataProduct"), function(dataProduct,
     # Format the info as a data.frame
     # Convert info list to dataframe, with properties as the headers. We always need to update the info so the query flag is updated correctly
     info <- data.frame(t(json$info[-1]))
-
+    
     # check if we should run the query again, if so, sleep for a short duration
     query = (autoPage && info$moreRows[[1]])
     if (query) {
@@ -923,20 +929,20 @@ setMethod("rds.select", signature("rds.dataProduct"), function(dataProduct,
 #' @rdname rds.tabulate
 #' @exportMethod rds.tabulate
 setGeneric("rds.tabulate", function(dataProduct,
-                                dimensions,
-                                measures = NULL,
-                                limit = NULL,
-                                offset = NULL,
-                                orderby = NULL,
-                                where = NULL,
-                                totals = TRUE,
-                                inject = FALSE)
+                                    dimensions,
+                                    measures = NULL,
+                                    limit = NULL,
+                                    offset = NULL,
+                                    orderby = NULL,
+                                    where = NULL,
+                                    totals = TRUE,
+                                    inject = FALSE)
   standardGeneric("rds.tabulate"))
 
 #' Tabulate
 #'
 #' The rds.tabulate(dataProduct) method is used to create aggregated tables and perform analysis on a data product.
-#' 
+#'
 #' @import jsonlite urltools
 #' @param dataProduct The dataProduct whose data is being used in the tabulation.
 #' @param dimensions The names of the variables that should be used as dimensions.
@@ -948,14 +954,14 @@ setGeneric("rds.tabulate", function(dataProduct,
 #' @param totals Specifies if totals should be included. Totals are used to provide roll up information about the counts of dimensions at different levels.
 #' @param inject Specifies if metadata should be injected into the data frame. If true and there are classifications available the columns codes will be replaced with code values. Defaults to FALSE
 setMethod("rds.tabulate", signature("rds.dataProduct", "character"), function(dataProduct,
-                                                                          dimensions,
-                                                                          measures = NULL,
-                                                                          limit = NULL,
-                                                                          offset = NULL,
-                                                                          orderby = NULL,
-                                                                          where = NULL,
-                                                                          totals = TRUE,
-                                                                          inject = FALSE) {
+                                                                              dimensions,
+                                                                              measures = NULL,
+                                                                              limit = NULL,
+                                                                              offset = NULL,
+                                                                              orderby = NULL,
+                                                                              where = NULL,
+                                                                              totals = TRUE,
+                                                                              inject = FALSE) {
   # Get the catalog from the data product
   catalog <- dataProduct@catalog
   
@@ -977,12 +983,12 @@ setMethod("rds.tabulate", signature("rds.dataProduct", "character"), function(da
   
   # Append any filled out options to the request
   paramPrefix <- "?"
-    tabulate <- paste(tabulate,
-                      paramPrefix,
-                      "metadata=true",
-                      sep = "",
-                      collapse = NULL)
-    paramPrefix <- "&"
+  tabulate <- paste(tabulate,
+                    paramPrefix,
+                    "metadata=true",
+                    sep = "",
+                    collapse = NULL)
+  paramPrefix <- "&"
   
   if (inject) {
     tabulate <- paste(tabulate,
@@ -1003,12 +1009,14 @@ setMethod("rds.tabulate", signature("rds.dataProduct", "character"), function(da
   }
   
   if (!is.null(dimensions)) {
-    tabulate <- paste(tabulate,
-                      paramPrefix,
-                      "dims=",
-                      url_encode(dimensions),
-                      sep = "",
-                      collapse = NULL)
+    tabulate <- paste(
+      tabulate,
+      paramPrefix,
+      "dims=",
+      url_encode(paste(dimensions, collapse = ",")),
+      sep = "",
+      collapse = NULL
+    )
     paramPrefix <- "&"
   }
   
@@ -1017,7 +1025,7 @@ setMethod("rds.tabulate", signature("rds.dataProduct", "character"), function(da
       tabulate,
       paramPrefix,
       "measure=",
-      url_encode(measures),
+      url_encode(paste(measures, collapse = ",")),
       sep = "",
       collapse = NULL
     )
@@ -1025,14 +1033,12 @@ setMethod("rds.tabulate", signature("rds.dataProduct", "character"), function(da
   }
   
   if (!is.null(limit))  {
-    tabulate <- paste(
-      tabulate,
-      paramPrefix,
-      "limit=",
-      limit,
-      sep = "",
-      collapse = NULL
-    )
+    tabulate <- paste(tabulate,
+                      paramPrefix,
+                      "limit=",
+                      limit,
+                      sep = "",
+                      collapse = NULL)
     paramPrefix <- "&"
   }
   
@@ -1065,7 +1071,7 @@ setMethod("rds.tabulate", signature("rds.dataProduct", "character"), function(da
         tabulate,
         paramPrefix,
         "orderby=",
-        url_encode(orderby),
+        url_encode(paste(orderby, collapse = ",")),
         sep = "",
         collapse = NULL
       )
